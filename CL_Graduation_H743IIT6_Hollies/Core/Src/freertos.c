583c96f2825ca9a35541fd634c23400e61d39592
DragonYH <1016633827@qq.com>
2025-05-06 01:06:02 +0800

    添加获取电压电流函数和计算有效值函数

--- CL_Graduation_H743IIT6_Hollies/Core/Src/freertos.c
+++ CL_Graduation_H743IIT6_Hollies/Core/Src/freertos.c
@@ -56,37 +56,37 @@
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "defaultTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for letterShellTask */
 osThreadId_t letterShellTaskHandle;
 const osThreadAttr_t letterShellTask_attributes = {
-  .name = "letterShellTask",
-  .stack_size = 1024 * 4,
-  .priority = (osPriority_t) osPriorityBelowNormal7,
+    .name = "letterShellTask",
+    .stack_size = 1024 * 4,
+    .priority = (osPriority_t)osPriorityBelowNormal7,
 };
 /* Definitions for chipTemperature */
 osThreadId_t chipTemperatureHandle;
 const osThreadAttr_t chipTemperature_attributes = {
-  .name = "chipTemperature",
-  .stack_size = 512 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "chipTemperature",
+    .stack_size = 512 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for oledDisplayTask */
 osThreadId_t oledDisplayTaskHandle;
 const osThreadAttr_t oledDisplayTask_attributes = {
-  .name = "oledDisplayTask",
-  .stack_size = 512 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "oledDisplayTask",
+    .stack_size = 512 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for keyTask */
 osThreadId_t keyTaskHandle;
 const osThreadAttr_t keyTask_attributes = {
-  .name = "keyTask",
-  .stack_size = 256 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "keyTask",
+    .stack_size = 256 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -104,16 +104,18 @@ extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
-void MX_FREERTOS_Init(void) {
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
+void MX_FREERTOS_Init(void)
+{
   /* USER CODE BEGIN Init */
   ad7606_Init();
   MX_USB_DEVICE_Init();
   userShellInit();
-  pll_Init_V(&signal_V, 50, 20000);
+  three_Phase_Init_V(&signal_V, 50, 20000);
+  three_Phase_Init_I(&signal_I, 50, 20000);
   ad7606_Start(&htim2, TIM_CHANNEL_1);
   /* USER CODE END Init */
 
@@ -138,7 +140,7 @@ void MX_FREERTOS_Init(void) {
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* creation of letterShellTask */
-  letterShellTaskHandle = osThreadNew(shellTask, (void*) &shell, &letterShellTask_attributes);
+  letterShellTaskHandle = osThreadNew(shellTask, (void *)&shell, &letterShellTask_attributes);
 
   /* creation of chipTemperature */
   chipTemperatureHandle = osThreadNew(StartChipTemperatureTask, NULL, &chipTemperature_attributes);
@@ -156,7 +158,6 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_EVENTS */
   /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
-
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -260,4 +261,3 @@ __weak void StartKeyTask(void *argument)
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
-
--- CL_Graduation_H743IIT6_Hollies/User/Src/user_interrupt.c
+++ CL_Graduation_H743IIT6_Hollies/User/Src/user_interrupt.c
@@ -4,6 +4,10 @@
 #include "tim.h"
 #include "spi.h"
 #include "user_global.h"
+#include "arm_math.h"
+
+static void getVoltageCurrent(void);
+static void calcEffectiveValue(void);
 
 /**
  * @brief    外部中断
@@ -14,15 +18,64 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     if (GPIO_Pin == AD7606_BUSY_Pin)
     {
-        // 读取AD7606数据
-        float adcValue[8] = {0};
-        ad7606_GetValue(&hspi2, 8, adcValue);
+        getVoltageCurrent();  // 获取电压电流
+        calcEffectiveValue(); // 计算有效值
+    }
+}
 
-        float Uab = adcValue[2];
-        float Ubc = adcValue[4];
-        float Uca = adcValue[6];
-        signal_V->basic->input_a = Uab;
-        signal_V->basic->input_b = Ubc;
-        signal_V->basic->input_c = Uca;
+/**
+ * @brief  获取相电压和电流
+ */
+static void getVoltageCurrent(void)
+{
+    // 读取AD7606数据
+    float adcValue[8] = {0};
+    ad7606_GetValue(&hspi2, 7, adcValue);
+
+    // 处理电压数据，将线电压转为相电压
+    float Uab = adcValue[2];
+    float Ubc = adcValue[4];
+    float Uca = adcValue[6];
+
+    signal_V->basic->input_a = (Uab - Uca) / 3.f;
+    signal_V->basic->input_b = (Ubc - Uab) / 3.f;
+    signal_V->basic->input_c = (Uca - Ubc) / 3.f;
+
+    // 处理电流数据
+    signal_I->basic->input_a = adcValue[1] * 2.178571f;
+    signal_I->basic->input_b = adcValue[3] * 2.250774f;
+    signal_I->basic->input_c = adcValue[5] * 2.172956f;
+}
+
+/**
+ * @brief  计算电压电流有效值
+ */
+static void calcEffectiveValue(void)
+{
+    static uint16_t cnt = 0;
+
+    static float V_rms_a[400] = {0};
+    static float V_rms_b[400] = {0};
+    static float V_rms_c[400] = {0};
+    static float I_rms_a[400] = {0};
+    static float I_rms_b[400] = {0};
+    static float I_rms_c[400] = {0};
+
+    V_rms_a[cnt] = signal_V->basic->input_a;
+    V_rms_b[cnt] = signal_V->basic->input_b;
+    V_rms_c[cnt] = signal_V->basic->input_c;
+    I_rms_a[cnt] = signal_I->basic->input_a;
+    I_rms_b[cnt] = signal_I->basic->input_b;
+    I_rms_c[cnt] = signal_I->basic->input_c;
+
+    if (++cnt == 400)
+    {
+        cnt = 0;
+        arm_rms_f32(V_rms_a, 400, &signal_V->basic->rms_a);
+        arm_rms_f32(V_rms_b, 400, &signal_V->basic->rms_b);
+        arm_rms_f32(V_rms_c, 400, &signal_V->basic->rms_c);
+        arm_rms_f32(I_rms_a, 400, &signal_I->basic->rms_a);
+        arm_rms_f32(I_rms_b, 400, &signal_I->basic->rms_b);
+        arm_rms_f32(I_rms_c, 400, &signal_I->basic->rms_c);
     }
 }
